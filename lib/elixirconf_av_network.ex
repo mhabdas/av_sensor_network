7596e0a0fb8dbe620614f4a2c340f323a20b1c51
Marta <marta.habdas@gmail.com>
2026-02-01 11:39:07 +0100

    Initial commit

---
+++ lib/elixirconf_av_network.ex
@@ -0,0 +1,18 @@
+defmodule ElixirconfAvNetwork do
+  @moduledoc """
+  Documentation for `ElixirconfAvNetwork`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirconfAvNetwork.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixirconf_av_network_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirconfAvNetworkTest do
+  use ExUnit.Case
+  doctest ElixirconfAvNetwork
+
+  test "greets the world" do
+    assert ElixirconfAvNetwork.hello() == :world
+  end
+end
